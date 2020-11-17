f46c22f24d89d577c598d268699a2e8ccea8bd89
dzanis-codes <46906740+dzanis-codes@users.noreply.github.com>
2020-11-17 12:09:14 +0200

    Update createdb3 (2).py

--- createdb3 (2).py
+++ createdb3 (2).py
@@ -1,16 +1,14 @@
 
 
 import sqlite3
-conn = sqlite3.connect('result4.db')
+conn = sqlite3.connect('result02.db')
 
 c = conn.cursor()
 
 # Create table
 c.execute('''CREATE TABLE results
-           (name_id INTEGER PRIMARY KEY, id, text, stavs, location, platiba, type, cena, stamp)''')
+           (name_id INTEGER PRIMARY KEY, ad_id, ad_text, stavs, location, premise_m2, land_m2, house_type, cena, ad_link, ad_source, estate_type, transaction_type, timestamp)''')
 
-# Insert a row of data from other example
-#c.execute("INSERT INTO ticker VALUES (null, '2006-01-05','BUY','RHAT',100,35.14)")
 
 # Save (commit) the changes
 conn.commit()
