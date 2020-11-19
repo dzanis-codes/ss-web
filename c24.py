2e41a28105e7b8d9024d3f87bc76d081d53201ab
dzanis-codes <46906740+dzanis-codes@users.noreply.github.com>
2020-11-19 12:12:14 +0200

    Update c24.py

--- c24.py
+++ c24.py
@@ -2,25 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Firefox()
-driver.get('url')
+driver.get('https://www.city24.lv/lv/saraksts?fr=0&lang=lv&usp=true&ord=sort-date-desc')
 
-select = Select(driver.find_element_by_id('fruits01'))
+select = Select(driver.find_element_by_id('new-search__column new-search__column--w20 new-search__transaction-type'))
 
-# select by visible text
-select.select_by_visible_text('Banana')
 
-# select by value 
-select.select_by_value('1')
-
-=====
-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-
-b = webdriver.Firefox()
-
-# navigate to the page
-select = Select(b.find_element_by_id(....))
 print select.options
 print [o.text for o in select.options] # these are string-s
-select.select_by_visible_text(....)
+
+## select by visible text
+#select.select_by_visible_text('Banana')
+
+## select by value 
+#select.select_by_value('1')
+#https://stackoverflow.com/questions/46553793/selecting-a-value-from-drop-down-with-span-id-in-selenium-python
+
+=====
