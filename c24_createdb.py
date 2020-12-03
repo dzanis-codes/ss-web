939fd2e044c7d343b9a173aedfc141b28aea5ec1
dzanis-codes <46906740+dzanis-codes@users.noreply.github.com>
2020-12-03 11:48:44 +0200

    Update c24_createdb.py

--- c24_createdb.py
+++ c24_createdb.py
@@ -5,7 +5,7 @@ c = conn.cursor()
 
 # Create table
 c.execute('''CREATE TABLE results
-           (name_id INTEGER PRIMARY KEY, ad_id, apraksts, stavs, adrese, premise_m2, land_m2, cena, ad_link, ad_source, estate_type, istabas, timestamp)''')
+           (name_id INTEGER PRIMARY KEY, ad_id, apraksts, stavs, adrese, premise_m2, land_m2, cena, cena_m2, ad_link, ad_source, estate_type, istabas, timestamp)''')
 
 
 # Save (commit) the changes
