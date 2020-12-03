90fb96f0552272901dcb1cfe34e2a1d3dba6936b
dzanis-codes <46906740+dzanis-codes@users.noreply.github.com>
2020-12-03 11:26:30 +0200

    Create c24_createdb.py

---
+++ c24_createdb.py
@@ -0,0 +1,13 @@
+import sqlite3
+conn = sqlite3.connect('result01c24.db')
+
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE results
+           (name_id INTEGER PRIMARY KEY, ad_id, apraksts, stavs, adrese, premise_m2, land_m2, cena, ad_link, ad_source, estate_type, istabas, timestamp)''')
+
+
+# Save (commit) the changes
+conn.commit()
+conn.close()
