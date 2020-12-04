0f83a3e7c908b847a38389b01d9a02d8117728f2
dzanis-codes <46906740+dzanis-codes@users.noreply.github.com>
2020-12-04 16:19:10 +0200

    Update rimi_createdb.py

--- rimi_createdb.py
+++ rimi_createdb.py
@@ -1,4 +1,16 @@
 
+import sqlite3
+conn = sqlite3.connect('result01rimi.db')
+
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE results
+           (name_id INTEGER PRIMARY KEY, produkta_id, prod_nosaukums, prod_links, prod_nosaukums2, prod_internal_data, cena, pilna_cena, discount, cena_pirms_atl, avots, timestamp)''')
 
 
-produkta_id, prod_nosaukums, prod_links, prod_nosaukums2, prod_internal_data, cena, pilna_cena, discount, cena_pirms_atl, avots, timestamp
+# Save (commit) the changes
+conn.commit()
+conn.close()
+
+
