24022ba2e29135fe181d047c3f36419060071e26
dzanis-codes <46906740+dzanis-codes@users.noreply.github.com>
2020-12-09 16:05:28 +0200

    Update webscr_c24_csv.py

--- webscr_c24_csv.py
+++ webscr_c24_csv.py
@@ -1,11 +1,28 @@
+
 import csv
+import sqlite3
 
-teksts = ("Žēburs", "čž")
+db_file = "C:\sqlite\db\pythonsqlite.db"
 
-with open("out.csv", 'w') as csv_file:
-    writer = csv.writer(csv_file, delimiter=';')
-    for row in teksts:
-        srow = []
-        for e in row:           
-            srow.append(e.encode('utf-8'))      
-        writer.writerows(srow)
+
+conn = sqlite3.connect(db_file)
+cur = conn.cursor()
+cur.execute("SELECT * FROM results")
+
+rows = cur.fetchall()
+
+for row in rows:
+    for entry in row:
+        print(row)
+        print(entry)
+
+
+
+
+##delimiterus labāk noteikt ;;
+csv.register_dialect('unixpwd', delimiter=';;', quoting=csv.QUOTE_NONE)
+f = open('test2.csv', 'w', newline='', encoding='utf-8')
+writer = csv.writer(f, 'unixpwd')
+writer.writerow(('ßßßüuhěelloworld',
+                 'hello bye'))
+f.close()
